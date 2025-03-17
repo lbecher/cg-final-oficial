59942cc009b4d976151dead8121bdf17eaa250a5
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2025-03-16 21:48:09 -0300

    adicionando dependências

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install -y curl build-essential gcc-mingw-w64-x86-64 binutils-mingw-w64-x86-64 pkg-config libssl-dev librust-glib-sys-dev librust-gdk-sys-dev 
+curl https://sh.rustup.rs -sSf | sh
 cargo build --target x86_64-pc-windows-gnu --release
 cargo build --target x86_64-unknown-linux-gnu --release
 mkdir -p bin
